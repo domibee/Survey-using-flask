dba18c7e92d68aba39ac6af65a2ae8bf421c2941
Nikki <dominiquelcruz@gmail.com>
2023-04-13 00:15:21 -0700

    sessions added to app.py

--- app.py
+++ app.py
@@ -1,45 +1,53 @@
-from flask import Flask, request, flash, render_template, redirect 
+from flask import Flask, request, flash, render_template, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "chickens"
+app.config['SECRET_KEY'] = "ssd3fg3465dfg"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-RESPONSES = []
+RESPONSES = 'responses'
 
 @app.route('/')
 def homepage():
     """Show home page of the survey"""
-
+    session[RESPONSES] = []
+    
     return render_template("home.html", survey=survey)
 
 @app.route('/begin', methods=["POST"])
 def start_survey():
+
+    session[RESPONSES] = []
     return redirect('/questions/0')
+
+@app.route('/answer', methods=["POST"])
+def answer():
+    
+    choice = request.form["answer"]
+    responses = session[RESPONSES]
+    responses.append(choice)
+    session[RESPONSES] = responses
+    return redirect(f'questions/{len(responses)}')
 
 @app.route('/questions/<int:qid>')
 def answer_questions(qid):
 
-    if (RESPONSES is None):
+    responses = session.get(RESPONSES)
+
+    if (responses is None):
         return redirect ('/')
     
-    if (len(RESPONSES) == len(survey.questions)):
+    if (len(responses) == len(survey.questions)):
         return redirect ('/complete')
     
-    if (len(RESPONSES) != qid):
+    if (len(responses) != qid):
         flash("Question accessed out of order")
-        return redirect(f'questions/{len(RESPONSES)}')
+        return redirect(f'/questions/{len(responses)}')
     
     question = survey.questions[qid]
     return render_template('questions.html',question=question, question_num=qid)
-
-@app.route('/answer', methods=["POST"])
-def answer():
-    choice = request.form["answer"]
-    RESPONSES.append(choice)
-    return redirect(f'questions/{len(RESPONSES)}')
 
 @app.route('/complete')
 def complete():
